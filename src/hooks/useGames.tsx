5f1196f9960f13646a8dfc92463986cdd3e2bf7f
sakib098 <79166304+sakib098@users.noreply.github.com>
2023-12-29 02:54:51 +0600

    creating game card

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
-interface Game {
+export interface Game {
   id: string;
   name: string;
+  background_image: string;
 }
 interface FetchGameResponses {
   count: number;
